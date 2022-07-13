15ef25f4437d2bea4e9bf69582038dc65b7f701c
princeardayfio <princeardayfio@yahoo.com>
2022-07-13 23:56:17 +0000

    [File]: assignment submission

--- src/assignment.js
+++ src/assignment.js
@@ -14,7 +14,11 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+	let sum = 0;
+	for (let i =0; i < arrayOfNumbers.length; i++) {
+	     sum = sum + arrayOfNumbers[i]
+	}
+    return sum
 }
 // assignment.sumOfNumbers = sumOfNumbers;
 
